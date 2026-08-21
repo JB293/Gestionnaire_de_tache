454af9bd168cefbf2b1b7b853453e87d9320c9a6
JB293 <eblinbj@gmail.com>
2026-08-21 18:29:38 +0000

    Ajout de la classe MainActivity avec les fonctions GreetingText et GreetingImage pour l'affichage des tâches.

---
+++ app/src/main/java/com/example/gestionnairedetaches/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.example.gestionnairedetaches
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.gestionnairedetaches.ui.theme.GestionnaireDeTachesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GestionnaireDeTachesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GreetingImage(
+                        titre = stringResource(R.string.all_tasks_text),
+                        sousTitre = stringResource(R.string.nice_text),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(titre: String, sousTitre: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = titre,
+            fontWeight = FontWeight.Bold,
+
+            modifier = Modifier.padding(
+                top = 24.dp,
+                bottom = 8.dp
+            )
+        )
+        Text(
+            text = sousTitre,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(titre: String, sousTitre: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = image,
+                contentDescription = null
+            )
+            GreetingText(
+                titre = titre,
+                sousTitre = sousTitre
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    GestionnaireDeTachesTheme {
+        GreetingImage(
+            titre = stringResource(R.string.all_tasks_text),
+            sousTitre = stringResource(R.string.nice_text)
+        )
+    }
+}
